73335213994fe5e7dd4686679209955cfb4d42f1
Christoph Knauf <christoph.knauf@t-online.de>
2018-12-17 22:08:49 +0100

    Reworked cli

--- sat/cli.py
+++ sat/cli.py
@@ -5,7 +5,7 @@ import argparse
 import os
 from argparse import RawTextHelpFormatter
 
-_DEFAULT_IGNORED_PATH__SEGMENTS = ["bin", "target", "examples", "test"]
+_DEFAULT_IGNORED_PATH_SEGMENTS = ["bin", "target", "examples", "test"]
 _VERSION = '%(prog)s 0.1.0'
 
 class Cli:
@@ -16,14 +16,17 @@ class Cli:
         self._parser = argparse.ArgumentParser(
             formatter_class=RawTextHelpFormatter)
         analyser_names = [analyser.name() for analyser in analysers]
-        required = self._parser.add_argument_group('required named arguments')
-        required.add_argument('-a', dest='analysers', nargs='+',
-                              choices=analyser_names, required=True, help='List of analysis to run')
-        self._parser.add_argument('-w', dest='workingdir', default=os.getcwd(),
+        subParsers = self._parser.add_subparsers(dest='cmd', metavar='analysis type')
+        subParsers.required = True
+        depsAnalysis = subParsers.add_parser("deps", help='Dependency analysis')
+        requiredForDeps = depsAnalysis.add_argument_group()
+        requiredForDeps.add_argument('-a', dest='abstractions', metavar='abstraction level', nargs='+', action='append',
+                              choices=analyser_names, required=True, help='List of abstraction levels the analysis should run for')
+        depsAnalysis.add_argument('-w', dest='workingdir' , metavar='workingdir', default=os.getcwd(),
                                   help='Root folder for recursive analysis. Default is script location')
-        self._parser.add_argument('-i', dest='ignored_path_segments', default=_DEFAULT_IGNORED_PATH__SEGMENTS, nargs='*',
-                                  help="List of ignored path segements. Default is " +
-                                  ", ".join(_DEFAULT_IGNORED_PATH__SEGMENTS) +
+        depsAnalysis.add_argument('-i', dest='ignored', metavar='ignored path segments', default=_DEFAULT_IGNORED_PATH_SEGMENTS, nargs='*',
+                                  help="List of ignored path segements. Defaults: " +
+                                  ", ".join(_DEFAULT_IGNORED_PATH_SEGMENTS) +
                                   ". Provide empty list to include all paths"
                                   )
         self._parser.add_argument(
@@ -31,4 +34,4 @@ class Cli:
 
     def parse(self):
         args = self._parser.parse_args()
-        return args.workingdir, args.ignored_path_segments, args.analysers
+        return args.workingdir, args.ignored, args.abstractions
